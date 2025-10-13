b3496e887af61616b12cda546cc386cfaf74ea64
Sujaljaiswal25 <sujaljaiswal304@gmail.com>
2025-10-13 20:45:13 +0530

    feat: initialize product  microservices API with CRUD operations and image upload functionality

---
+++ product/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const app = require("./src/app");
+const connectToDB = require("./src/db/db");
+
+connectToDB();
+
+
+
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+})
